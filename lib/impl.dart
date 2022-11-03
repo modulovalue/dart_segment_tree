41e35cb9f5f5affc8de5b68fc639c563e87ed44d
Modestas Valauskas <valauskasmodestas@gmail.com>
2022-11-03 22:33:30 +0100

    project structure

---
+++ lib/impl.dart
@@ -0,0 +1,11 @@
+// region public
+// TODO factory
+
+abstract class SegmentTree<V> {
+  // TODO interface
+}
+// endregion
+
+// region internal
+// TODO impl
+// endregion
---
+++ test/segment_tree_test.dart
@@ -0,0 +1,15 @@
+import 'package:test/test.dart';
+
+void main() {
+  group("segment tree", () {
+    group("construction", () {
+      // TODO
+    });
+    group("query", () {
+      // TODO
+    });
+    group("update", () {
+      // TODO
+    });
+  });
+}
